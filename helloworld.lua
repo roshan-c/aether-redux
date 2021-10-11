df237cdcb1989c076edfc1319ef585bcbeef03a0
roshan-c <hhhhhhhhh333365@gmail.com>
2021-10-11 19:52:54 +0100

    Create helloworld.lua

---
+++ helloworld.lua
@@ -0,0 +1,11 @@
+-- This is how you comment
+-- This is how you define a local variable, otherwise it is global
+local a = ("A says Hello World")
+-- Printing works the exact same as Python, yet to see a change
+print("Hello World")
+print(a)
+-- This is how you define a function, you must type end at the end
+function b()
+    print("B says Hello World")
+end
+b()
